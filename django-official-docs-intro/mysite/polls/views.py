e647b083b74fee824fdd79f1f1b3dc8e9da7c591
Tomasz Nowakowski <toalak@gmail.com>
2023-04-19 22:24:12 +0200

    Part 5 finished

--- django-official-docs-intro/mysite/polls/views.py
+++ django-official-docs-intro/mysite/polls/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.utils import timezone
 from django.views import generic
 
 from .models import Choice, Question
@@ -15,8 +16,13 @@ class IndexView(generic.ListView):
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by("-pub_date")[:5]
+        """
+        Return the last five published questions (not including those
+        set to be published in the future).
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by(
+            "-pub_date"
+        )[:5]
 
 
 class DetailView(generic.DetailView):
@@ -45,9 +51,4 @@ def vote(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        return HttpResponseRedirect(
-            reverse(
-                "polls:results",
-                args=(question.id,)
-            )
-        )
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
